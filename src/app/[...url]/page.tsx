821f79fc488ac0a4a3fae6ec7c2a7fe91c1ebc4c
dealbisac <chand.dipendra19@gmail.com>
2024-08-12 15:28:57 +0200

    feat: create refined url component to remove extra spaces

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,10 +1,30 @@
+import { ragChat } from "@/lib/rag-chat"
+
 interface PageProps {
     params: {
         url: string | string[] | undefined 
     }
 }
+
+function refineUrl({ url }: { url: string[] }) {
+    const decodedComponent = url.map((component) => decodeURIComponent(component));
+
+    if (decodedComponent[decodedComponent.length - 1] === "") {
+        decodedComponent.pop();
+    }
+
+    return decodedComponent.join("/")
+}
+
 const Page = ({ params } : PageProps) => {
+  const refinedUrl = refineUrl({ url: params.url as string[] })
+
   console.log(params)
+
+  await ragChat.context.add({
+    type: "html",
+    source: 
+  })
   return (
     <div>Hello</div>
     
