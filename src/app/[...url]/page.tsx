81289732daf31378e2da0eb0877dfb4a55bf89a5
dealbisac <chand.dipendra19@gmail.com>
2024-08-12 19:48:32 +0200

    feat: update no vector db if url already indexed

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,4 +1,5 @@
 import { ragChat } from "@/lib/rag-chat"
+import { redis } from "@/lib/redis";
 
 interface PageProps {
     params: {
@@ -19,13 +20,24 @@ function refineUrl({ url }: { url: string[] }) {
 const Page = async ({ params } : PageProps) => {
   const refinedUrl = refineUrl({ url: params.url as string[] })
 
-  console.log(params)
+  // Check if the provided URL is already indexed so that new vector db can be duplicated
+  const isAlreadyIndexed = await redis.sismember("indexed-urls", refinedUrl);
+  
+  if (!isAlreadyIndexed) {
 
-  await ragChat.context.add({
-    type: "html",
-    source: refinedUrl,
-    config: { chunkOverlap:50, chunkSize:200 },
-  })
+    // perform ragChat
+    await ragChat.context.add({
+        type: "html",
+        source: refinedUrl,
+        config: { chunkOverlap:50, chunkSize:200 },
+      })
+
+    // Add the URL to the indexed-urls set
+    await redis.sadd("indexed-urls", refinedUrl);
+  }
+
+  // console.log(params)
+
   return (
     <div>Hello</div>
     
