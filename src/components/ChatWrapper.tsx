21448254aa3579a88c306b6678ff90262c461c65
dealbisac <chand.dipendra19@gmail.com>
2024-08-14 18:47:02 +0200

    add handle submit option to implement action while submitting

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -3,15 +3,22 @@
 import { useChat } from "ai/react"
 
 const ChatWrapper = ({sessionId} : { sessionId: string }) => {
-    const { messages } = useChat({
+    // const [input, seInput] = useState("")
+
+    const { messages, handleInputChange, handleSubmit, input} = useChat({
         api: "/api/chat-stream",
         body: {sessionId},
     })
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
         <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
-            ChatWrapper
+            {JSON.stringify(messages)}
         </div>
+
+        <form onSubmit={handleSubmit}>
+            <input value={input} onChange={handleInputChange} type="text" />
+        </form>
+        
     </div>
   )
 }
