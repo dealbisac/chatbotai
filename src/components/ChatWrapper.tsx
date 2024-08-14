d9591297e0ceddaaceca1dd235eb7c5bcd86ed62
dealbisac <chand.dipendra19@gmail.com>
2024-08-14 18:25:07 +0200

    implement ai in chatwrapper

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -1,8 +1,18 @@
 "use client"
 
-const ChatWrapper = () => {
+import { useChat } from "ai/react"
+
+const ChatWrapper = ({sessionId} : { sessionId: string }) => {
+    const { messages } = useChat({
+        api: "/api/chat-stream",
+        body: {sessionId},
+    })
   return (
-    <div>ChatWrapper</div>
+    <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
+        <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
+            ChatWrapper
+        </div>
+    </div>
   )
 }
 
