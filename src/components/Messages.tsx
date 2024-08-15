a73ac938ea88cbb70715df692a57ad629a22068f
dealbisac <chand.dipendra19@gmail.com>
2024-08-16 00:26:21 +0200

    fix: update the info when there is no message.

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -8,7 +8,7 @@ interface MessagesProps {
 const Messages = ( {messages} : MessagesProps) => {
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
-        {messages? (messages.map((message, index) => (
+        {messages.length ? (messages.map((message, index) => (
             <Message 
                 key={index} 
                 content={message.content}
@@ -16,7 +16,7 @@ const Messages = ( {messages} : MessagesProps) => {
             />
             ))
         ):(
-        <div className="flex flex-1 flex-col items-center justify-center gap-2">
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
             <MessageCircleIcon className="size-8 text-blue-500" />
             <h3 className="font-semibold text-xl text-white">You&rsquo;re all set.</h3>
             <p className="text-zinc-500 text-sm">Start a conversation by typing a question.</p>
