3768a1a6b6dfa4a53c37a5e5e6cb3d1676255144
dealbisac <chand.dipendra19@gmail.com>
2024-08-15 23:43:24 +0200

    import single message component

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,16 +1,23 @@
 import { type Message as TMessage } from "ai/react"
+import Message from "./Message"
 
 interface MessagesProps {
     messages: TMessage[]
 }
 const Messages = ( {messages} : MessagesProps) => {
   return (
-    <div className="flex">
-        {messages.map((message) => (
-            <div key={message.id}>
-                {message.content}
-            </div>
-        ))}
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
+        {messages? messages.map((message, index) => (
+            <Message 
+                key={index} 
+                content={message.content}
+                isUserMessage={message.role === "user"}
+            />
+        )):
+        (<div>
+            No messages
+        </div>
+        )}
     </div>
   )
 }
