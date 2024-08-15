d9fa04bc03fa46eb6007f4f1c6d263298fb7fbe7
dealbisac <chand.dipendra19@gmail.com>
2024-08-16 00:12:06 +0200

    implement the message when no conversation has been started.

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -1,5 +1,6 @@
 import { type Message as TMessage } from "ai/react"
 import Message from "./Message"
+import { MessageCircleIcon } from "lucide-react"
 
 interface MessagesProps {
     messages: TMessage[]
@@ -7,15 +8,18 @@ interface MessagesProps {
 const Messages = ( {messages} : MessagesProps) => {
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
-        {messages? messages.map((message, index) => (
+        {messages? (messages.map((message, index) => (
             <Message 
                 key={index} 
                 content={message.content}
                 isUserMessage={message.role === "user"}
             />
-        )):
-        (<div>
-            No messages
+            ))
+        ):(
+        <div className="flex flex-1 flex-col items-center justify-center gap-2">
+            <MessageCircleIcon className="size-8 text-blue-500" />
+            <h3 className="font-semibold text-xl text-white">You&rsquo;re all set.</h3>
+            <p className="text-zinc-500 text-sm">Start a conversation by typing a question.</p>
         </div>
         )}
     </div>
