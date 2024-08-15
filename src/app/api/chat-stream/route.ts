fbfc620a4e20fad4d4f0f34b3b31841ac40c71f4
dealbisac <chand.dipendra19@gmail.com>
2024-08-15 23:16:53 +0200

    add comments to increase readibility

--- src/app/api/chat-stream/route.ts
+++ src/app/api/chat-stream/route.ts
@@ -5,10 +5,13 @@ import { NextRequest } from "next/server";
 export const POST = async (req: NextRequest) => {
     const { messages, sessionId } = await req.json()
 
+    // get the content of the last message
     const lastMessage = messages[messages.length - 1].content
 
+    // get the response from ragChat of the last message
     const response = await ragChat.chat(lastMessage, { streaming: true, sessionId })
 
+    // return the response
     return aiUseChatAdapter(response);
 
     // console.log("response", response)
