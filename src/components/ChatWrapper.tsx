7ad6200569f699607c2d1d92f8427df598c83888
dealbisac <chand.dipendra19@gmail.com>
2024-08-15 23:28:29 +0200

    import Message component

--- src/components/ChatWrapper.tsx
+++ src/components/ChatWrapper.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useChat } from "ai/react"
+import Messages from "./Messages"
 
 const ChatWrapper = ({sessionId} : { sessionId: string }) => {
     // const [input, seInput] = useState("")
@@ -12,7 +13,8 @@ const ChatWrapper = ({sessionId} : { sessionId: string }) => {
   return (
     <div className="relative min-h-full bg-zinc-900 flex divide-y divide-zinc-700 flex-col justify-between gap-2">
         <div className="flex-1 text-black bg-zinc-800 justify-between flex flex-col">
-            {JSON.stringify(messages)}
+            {/* {JSON.stringify(messages)} */}
+            <Messages messages={messages} />
         </div>
 
         <form onSubmit={handleSubmit}>
