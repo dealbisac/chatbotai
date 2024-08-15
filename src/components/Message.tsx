d4fb81278ff42711e71970b8d9b4da6a46352428
dealbisac <chand.dipendra19@gmail.com>
2024-08-16 00:11:42 +0200

    implement the different icons for user and ai for responding message

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,3 +1,6 @@
+import { cn } from "@/lib/utils"
+import { Bot, User } from "lucide-react"
+
 interface MessagesProps {
     content: string,
     isUserMessage: boolean
@@ -5,7 +8,26 @@ interface MessagesProps {
 
 const Message = ({ content, isUserMessage}: MessagesProps) => {
   return (
-    <div>Message</div>
+    <div
+        className={cn({
+            "bg-zinc-800 text-white": isUserMessage,
+            "bg-zinc-800/25 text-black": !isUserMessage
+        })}
+    >
+        <div className="p-6">
+            <div className="max-w-3xl mx-auto flex items-start gap-2.5">
+                <div className={cn("size-10 shrink-0 aspect-square rounded-full border border-zinc-700 bg-zinc-900 flex justify-center items-center", 
+                {
+                    "bg-blue-950 border-blue-700 text-zinc-200": isUserMessage,
+                }
+                )}>
+                    {isUserMessage ? <User className="size-5" /> : <Bot className="size-5 text-white" />}
+
+                </div>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
