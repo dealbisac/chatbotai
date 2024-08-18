6e4bdeb925a81dbb0bed108acba8f42347681e17
dealbisac <chand.dipendra19@gmail.com>
2024-08-18 17:06:14 +0200

    declare input props for chatInput component

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+"use client"
 
-const ChatInput = () => {
+import { Button, Textarea } from "@nextui-org/react"
+import { Send } from "lucide-react"
+import { type useChat } from "ai/react"
+
+// Custom types for generic types
+type HandleInputChange = ReturnType<typeof useChat>["handleInputChange"]
+type HandleSubmit = ReturnType<typeof useChat>["handleSubmit"]
+type SetInput = ReturnType<typeof useChat>["setInput"]
+
+interface ChatInputProps {
+    input: string
+    handleInputChange: HandleInputChange
+    handleSubmit: HandleSubmit
+    setInput: SetInput
+}
+
+const ChatInput = ({}: ChatInputProps) => {
   return (
     <div>ChatInput</div>
   )
