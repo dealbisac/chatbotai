baf164e044b40f3afe7d21a37469ce460c7a5007
dealbisac <chand.dipendra19@gmail.com>
2024-08-18 22:18:56 +0200

    feat: set cookies, generate if not availbale and return the response

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,4 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
     const res = NextResponse.next();
+
+    const cookie = req.cookies.get("sessionId")
+
+    if (!cookie) {
+        res.cookies.set("sessionId", Date.now().toString() + crypto.randomUUID(), {
+            maxAge: 60 * 60 * 24 * 365,
+        })
+    }
+
+    return res
 }
